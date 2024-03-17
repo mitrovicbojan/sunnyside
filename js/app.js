e191e4bdb599021514b5cb666d90f240e000b349
unknown <mitrovicbojan87@gmail.com>
2024-03-17 16:25:16 +0200

    nav

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 const btnNavEl = document.querySelector(".btn");
 const menu = document.querySelector(".main-nav");
+const allLinks = document.querySelectorAll("a:link");
 
 btnNavEl.addEventListener("click", () => {
   menu.classList.toggle("nav-open");
+});
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", (e) => {
+    if (link.classList.contains("main-nav-link"))
+      menu.classList.toggle("nav-open");
+  });
 });
